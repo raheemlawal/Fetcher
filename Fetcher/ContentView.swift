ba41eef7f07e03e24928c6fe04589fbf6eb156a2
Raheem Lawal <lawalraheem@gmail.com>
2024-02-21 01:41:29 -0800

    create app

--- Fetcher/ContentView.swift
+++ Fetcher/ContentView.swift
@@ -13,40 +13,91 @@ struct ContentView: View {
 
     @State private var showImmersiveSpace = false
     @State private var immersiveSpaceIsShown = false
+    
+    @State private var userFirstName: String = ""
+    @State private var fetchTitle: String = ""
+    @State private var fetchURL: String = ""
+    @State private var userIdInput: String = ""
+    
+    struct User: Decodable {
+        let id: Int8
+        let created_at: String
+        let first_name: String
+        let favorite_color: String
+    }
+    
+    func submitQuery() {
+        print("hello")
+        Task {
+            do {
+                let user = try await fetchUserFirstNameFromAPI(userid: userIdInput)
+
+                userFirstName = user.first_name
+                fetchTitle = "Fetched from API."
+                fetchURL = "Visit https://fastapi-production-3b37.up.railway.app/"
+                
+                
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func fetchUserFirstNameFromAPI(userid: String) async throws -> User {
+        let url = URL(string: "https://dummyapi-production-b5d9.up.railway.app/user/\(userid)")!
+
+        let (data, _) = try await URLSession.shared.data(from: url)
+
+        let decoded = try JSONDecoder().decode(User.self, from: data)
+
+        return decoded
+    }
 
     @Environment(\.openImmersiveSpace) var openImmersiveSpace
     @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
 
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
+            
+            Text("Fetcher App")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .font(.system(size: 60))
+                .padding(.bottom)
+            
+            Text("Return first name of a specific user from database.")
+                .fontWeight(.light)
+                .font(.system(size: 30))
+                .padding(.bottom)
+            
+            TextField("Enter User ID", text: $userIdInput)
+                .frame(maxWidth: 200, minHeight: 70)
+                .multilineTextAlignment(.center)
+                .border(.white)
+            
+            Button(action: submitQuery, label: {
+                            Text("Submit")
+                                .fontWeight(.heavy)
+                                .font(.system(size: 25))
+                        })
+                        .frame(minWidth: 300, minHeight: 70)
+                        .background(.red)
+                        .cornerRadius(10)
+                        .padding()
+            
+            Text("\(userFirstName)")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .font(.system(size: 60))
+                .padding(.bottom)
+            
+            Text("\(fetchTitle)")
+                .font(.system(size: 20))
+                .padding(.bottom)
+            
+            Text("\(fetchURL)")
+                .font(.system(size: 20))
 
-            Text("Hello, world!")
-
-            Toggle("Show Immersive Space", isOn: $showImmersiveSpace)
-                .toggleStyle(.button)
-                .padding(.top, 50)
         }
         .padding()
-        .onChange(of: showImmersiveSpace) { _, newValue in
-            Task {
-                if newValue {
-                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
-                    case .opened:
-                        immersiveSpaceIsShown = true
-                    case .error, .userCancelled:
-                        fallthrough
-                    @unknown default:
-                        immersiveSpaceIsShown = false
-                        showImmersiveSpace = false
-                    }
-                } else if immersiveSpaceIsShown {
-                    await dismissImmersiveSpace()
-                    immersiveSpaceIsShown = false
-                }
-            }
-        }
     }
 }
 
